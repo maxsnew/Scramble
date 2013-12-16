56941c0e1571be865d56509c64edaaf121b1d225
Max New <maxsnew@gmail.com>
2013-12-16 02:31:40 -0600

    Rotates the board (but update is still broken)

--- Scramble.elm
+++ Scramble.elm
@@ -5,9 +5,9 @@ import Input as I
 import String
 
 -- Main
-main = startGame startState
+main = startGame <| startState start5
 
-
+-- Boards are assumed to be nxn for some n
 type Board = [[Char]]
 type Position = { x : Int, y : Int }
 
@@ -15,49 +15,59 @@ type Position = { x : Int, y : Int }
 type GameState = { board    : Board
                  , curGuess : [Position] }
 
+data Rot = Clock | CtrClock           
+
 data Msg = Tile Position Char
          | Reset
+         | Rotate Rot
 
 -- Initial
-startBoard : Board
-startBoard = [ ['a', 'b', 'c']
-             , ['d', 'e', 'f']
-             , ['g', 'h', 'i'] ]
+start3 = [ ['a', 'b', 'c']
+         , ['d', 'e', 'f']
+         , ['g', 'h', 'i'] ]
 
-startState = { board = startBoard
-             , curGuess = []}             
+start5 = [ ['t', 'd', 'o', 'a', 'f']
+         , ['l', 'n', 't', 'c', 'n']
+         , ['x', 'e', 't', 'p', 'e']
+         , ['f', 'g', 'l', 't', 'v']
+         , ['f', 'd', 'e', 'a', 't'] ]
+
+startState b = { board = b, curGuess = []}             
 
 -- Update
 plainButton = fst . GI.button
+
+rotateR : [[a]] -> [[a]]
+rotateR b = let base = repeat (length b) []
+            in foldl (zipWith (::)) base b
+
+rotateL b = let base = repeat (length b) []
+            in foldr (zipWith (::)) base . map reverse <| b
 
 interpret : Msg -> GameState -> GameState
 interpret m g = case m of
   Tile p c -> squareClick p c g
   Reset    -> { g | curGuess <- [] }
+  Rotate r -> case r of
+    Clock    -> { g | board <- rotateR g.board }
+    CtrClock -> { g | board <- rotateL g.board }
 
 squareClick : Position -> Char -> GameState -> GameState
 squareClick pos c st = { st | curGuess <- consNew pos st.curGuess }
-
-member x xs = case xs of
-  []        -> False
-  (y :: ys) -> if (x == y)
-               then True
-               else member x ys
-
-consNew : Position -> [Position] -> [Position]
-consNew p ps = if member p ps
-               then ps
-               else p :: ps
 
 -- Display
 startGame : GameState -> Signal Element
 startGame init = 
   let btns         = I.buttons Nothing
       button m e = btns.button (Just m) e
-      state        = foldp (maybe id interpret) init (btns.events) in
-  flow down <~ combine [ renderBoard button <~ state
-                       , constant <| button Reset (plainButton "Reset")
-                       , asText <~ state]
+      state        = foldp (maybe id interpret) init (btns.events)
+      pButton m s = constant . button m . plainButton <| s
+  in
+   flow down <~ combine [ renderBoard button <~ state
+                        , pButton Reset          "Reset"
+                        , pButton (Rotate Clock) "Clockwise"
+                        , pButton (Rotate CtrClock) "Counter-Clockwise"
+                        , asText <~ state]
 
 renderBoard : (Msg -> Element -> Element) -> GameState -> Element
 renderBoard but = let mkBut p c =  but (Tile p c) (tileButton c) in 
@@ -70,6 +80,17 @@ tileButton c = let s = 50 in
               ]
 
 -- Utility
+member x xs = case xs of
+  []        -> False
+  (y :: ys) -> if (x == y)
+               then True
+               else member x ys
+
+consNew : Position -> [Position] -> [Position]
+consNew p ps = if member p ps
+               then ps
+               else p :: ps
+
 singleton : Char -> String
 singleton c = String.cons c ""
 
